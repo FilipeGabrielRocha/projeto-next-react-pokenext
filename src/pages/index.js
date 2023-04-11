a47079daa41c51191a5419e907d16e4ffc7bb6bd
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-04-11 08:38:13 -0300

    Atualização

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import styles from "@/styles/Home.module.css";
+
 import Image from "next/image";
+
+import Card from "@/components/Card";
 
 export async function getStaticProps() {
   const maxPokemons = 251; // Determinando o máximo de pokemons que a API vai voltar
@@ -37,7 +40,7 @@ export default function Home({ pokemons }) {
       </div>
       <div className={styles.pokemon_container}>
         {pokemons.map((pokemon) => (
-          <p key={pokemon.id}>{pokemon.name}</p>
+          <Card key={pokemon.id} pokemon={pokemon}/>
         ))}
       </div>
     </>
